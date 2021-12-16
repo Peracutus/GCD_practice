ac4fdfd73a064300f3c94d226f57f75fc13f9348
Roman <74953662+Peracutus@users.noreply.github.com>
2021-12-16 11:23:45 +0300

    Initial Commit

---
+++ GCD practice project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GCD practice project
+//
+//  Created by Roman on 16.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
